f216d3e8dfc0bc5e21780acff6fcbe79cea4fb19
Wheezyotter <jacobpohs@gmail.com>
2023-05-15 12:57:06 -0700

    First commit

---
+++ Data Engineering.sql
@@ -0,0 +1,54 @@
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+
+CREATE TABLE departments (
+	dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE titles (
+	title_id VARCHAR(5) PRIMARY KEY NOT NULL,
+	title VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employees (
+	emp_no INTEGER PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(5) NOT NUll,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NUll,
+	sex VARCHAR (1) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INTEGER NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INTEGER NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries (
+	emp_no INTEGER NOT NULL,
+	salary INTEGER NOT NULL,
+	PRIMARY KEY (emp_no, salary),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * 
+FROM salaries;
